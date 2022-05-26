8712aa4bb773fc6d672d4beb4c63d3c87ebe0e17
yunze <yunze.wydl@raycloud.com>
2022-05-26 20:35:40 +0800

    feat():增加配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,13 +1,17 @@
 /*
  * @Date: 2022-05-24 21:37:47
  * @LastEditors: yunze yunze.wydl@raycloud.com
- * @LastEditTime: 2022-05-26 20:10:19
+ * @LastEditTime: 2022-05-26 20:33:31
  * @Description: Do not edit
  * @FilePath: /l-lib/.umirc.ts
  */
 import { defineConfig } from 'dumi';
 import navs from './routes/navs';
+const repo = 'l-lib';
 export default defineConfig({
+  base: `/${repo}/`,
+  publicPath: `/${repo}/`,
+  exportStatic: {},
   title: 'l-lib',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
